0c0067fd3dfec56f0e5a92ab791ed058bbd07513
MichaelMikhieienko <bsmmmv@gmail.com>
2023-06-10 11:00:03 +0300

    Correct

--- src/index.js
+++ src/index.js
@@ -1,48 +1,29 @@
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
 const breedSelect = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
+const loaderElement = document.querySelector('.loader');
+const errorElement = document.querySelector('.error');
 
-
-function showLoader() {
-  loader.style.display = 'block';
-}
-
-function hideLoader() {
-  loader.style.display = 'none';
-}
-
-function showError() {
-  error.style.display = 'block';
-}
-
-function hideError() {
-  error.style.display = 'none';
-}
-
-  fetchBreeds()
-    .then(breeds => {
-
-      breedSelect.classList.remove("is-hidden");
-      loader.classList.add("is-hidden")
-      breeds.forEach(breed => {
-        const option = document.createElement('option');
-        option.value = breed.id;
-        option.textContent = breed.name;
-        breedSelect.appendChild(option);
-      });
-    })
-    .catch(error => {
-      loader.classList.add("is-hidden")
-      error.classList.remove("is-hidden")      
-    })    
-
+fetchBreeds()
+  .then(breeds => {
+    breedSelect.classList.remove('is-hidden');
+    loaderElement.classList.add('is-hidden');
+    breeds.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+    loaderElement.classList.add('is-hidden');
+    errorElement.classList.remove('is-hidden');
+  });
 
 const catInfo = document.querySelector('.cat-info');
 
 function updateCatInfo(cat) {
-  console.log(cat);
   catInfo.innerHTML = `
     <img src="${cat.url}" alt="${cat.breeds[0].name}" width="400px" />
     <h2>${cat.breeds[0].name}</h2>
@@ -53,7 +34,9 @@ function updateCatInfo(cat) {
 
 breedSelect.addEventListener('change', () => {
   const selectedBreedId = breedSelect.value;
-  showLoader();
+  loaderElement.classList.remove('is-hidden');
+  catInfo.classList.add('is-hidden');
+  errorElement.classList.add('is-hidden');
   fetchCatByBreed(selectedBreedId)
     .then(cats => {
       if (cats.length > 0) {
@@ -61,13 +44,12 @@ breedSelect.addEventListener('change', () => {
         updateCatInfo(cat);
       }
 
-      hideError();
+      catInfo.classList.remove('is-hidden');
+      loaderElement.classList.add('is-hidden');
     })
     .catch(error => {
       console.error('Error:', error.message);
-      showError();
-    })
-    .finally(() => {
-      hideLoader();
+      loaderElement.classList.add('is-hidden');
+      errorElement.classList.remove('is-hidden');
     });
 });
